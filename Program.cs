6bed88bcf12837951616f845c19c3caa8d199276
wasif farid <wsffarid1@gmail.com>
2021-09-02 13:58:01 +0530

    added dice roll

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace snakeladderprob
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to snake ladder simulation");
+            int rollDice;
+            Random random = new Random();
+            rollDice = random.Next(1, 7);
+            Console.WriteLine("Rolled Dice number is:" + rollDice);
         }
     }
 }
