e4ba853f79b0a937283479ad4c7624718adabe80
wasif farid <wsffarid1@gmail.com>
2021-09-02 13:53:36 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace snakeladderprob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to snake ladder simulation");
+        }
+    }
+}
