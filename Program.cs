71b624dc8f5e3597e2e0272930a30f58200239ea
wasif farid <wsffarid1@gmail.com>
2021-09-02 14:05:31 +0530

    added player position

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace snakeladderprob
 {
@@ -6,11 +10,32 @@ namespace snakeladderprob
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to snake ladder simulation");
-            int rollDice;
+
+            int playerPos = 0, rollDice;
+            //creates random number for rolling dice
             Random random = new Random();
             rollDice = random.Next(1, 7);
             Console.WriteLine("Rolled Dice number is:" + rollDice);
+            // print user option
+            Console.WriteLine("Player Option 1.No Play 2.Ladder 3.Snake");
+            var list = new List<string> { "No Play", "Ladder", "Snake" };
+            //check user option
+            int pos = random.Next(list.Count);
+            //condition based on user option
+            if (list[pos] == "Ladder")
+            {
+                playerPos += rollDice;
+
+            }
+            else if (list[pos] == "Snake")
+            {
+                playerPos -= rollDice;
+
+            }
+
+            //print result of playerposition and dice rolled
+            Console.WriteLine("Player option:" + list[pos]);
+            Console.WriteLine("player updated position:" + playerPos);
         }
     }
 }
