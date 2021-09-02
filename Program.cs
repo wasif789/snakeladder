d1422a39ab59de103b42961e0a0a924322cce9e5
wasif farid <wsffarid1@gmail.com>
2021-09-02 14:39:59 +0530

    ecact 100 position

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace snakeladderprob
         {
             //local variable
             int rollDice, playerPos = 0;
+            int count = 0;
 
             //To generate random number
             Random random = new Random();
@@ -26,12 +27,23 @@ namespace snakeladderprob
             Console.WriteLine("Player checks options 1.No Play  2.Ladder   3.Snake");
 
             //Use random to check user option
-            int index = random.Next(list.Count);
-            Console.WriteLine("Player option is : " + list[index]);
+            int pos = random.Next(list.Count);
+            Console.WriteLine("Player option is : " + list[pos]);
             if (playerPos + rollDice < 100)
             {
-                if (list[index] == "Ladder") playerPos += rollDice;
-                if (list[index] == "Snake") playerPos -= rollDice;
+                if (list[pos] == "Ladder")
+                {
+                    playerPos += rollDice;
+                }
+                if (list[pos] == "Snake")
+                {
+                    playerPos -= rollDice;
+                }
+            }
+            else if (playerPos + rollDice == 100 && list[pos] == "Ladder")
+            {
+                playerPos += rollDice;
+
             }
 
             if (playerPos < 0)
@@ -41,7 +53,9 @@ namespace snakeladderprob
             //Print to Console
             Console.WriteLine("\n");
             Console.WriteLine("Player current position : " + playerPos);
+            count++;
             Console.WriteLine("Final position is :" + playerPos);
+            Console.WriteLine("Total MOves:" + count);
         }
     }
 }
